3cc601a4a875e6362e55e13dfb17f1751089ab1e
calllivecn <calllivecn@outlook.com>
2025-10-20 05:16:49 +0800

    解锁多个luks分区ok

--- LUKSUkey/install.sh
+++ LUKSUkey/install.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/bash
 
-
-
-
 install -m400 usb-keyfile.conf "/etc/usb-keyfile.conf"
+install -m400 luks_uuid.conf "/etc/luks_uuid.conf"
 install -m500 usb-keyfile-hook "/etc/initramfs-tools/hooks/usb-keyfile"
 install -m500 usb-keyfile-unlock "/etc/initramfs-tools/scripts/local-top/usb-keyfile-unlock"
